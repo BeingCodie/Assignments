730f0276c8d824d41c1acac00149dd5341630a17
Abhishek <kabhi7235@gmail.com>
2024-09-27 00:34:59 +0530

    first

--- Array/Q2.cpp
+++ Array/Q2.cpp
@@ -1,9 +1,19 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-int main() {
-
-int num[]={1 , 2 };
-cout << num[1] << endl;
-return 0;
-
+int main()
+{
+    int n;
+    cout<<"enter the size of array : ";
+    cin >> n;
+    int num[n];
+    //entering value of array 
+    for ( int i = 0 ; i < n ; i ++){
+        cin >> num[i] ;
+    }
+    // printing value off array
+    for (int i = 0; i <= n - 1; i++){
+        cout << num[i] << ",";
+    }
+    cout << sizeof(num) << endl;
+    return 0;
 }
